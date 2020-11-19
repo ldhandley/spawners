024195224aea8f2c5a96b1fb9b2c0d675b8caa3b
srfoster <lindseyhandley@gmail.com>
2020-11-19 10:31:21 -0800

    started on lore

--- lore.rkt
+++ lore.rkt
@@ -3,14 +3,37 @@
 (require spawners
 	 codespells/lore)
 
+(require-mod fire-particles)
+(require-mod rocks)
+
 (define-rune-collection-lore 
-  #:name "A Cool Name for Spawners"
+  #:name "Spawning"
   #:description 
   @md{
-    A cool intro about Spawners
+  
   }
   #:rune-lores
-  (list)
+  (list
+    (rune-lore
+      #:name "Spawner"
+      #:rune (spawner-rune)
+      #:description @md{
+      This rune creates a spawner with two given parameters: (1) a rune to spawn and (2) a length of time in ms. If a 2nd parameter is not given, the default 1000 ms will be used.    
+
+      @(rune-code-example
+         (append-rune-langs #:name #f
+                            (hierarchy:my-mod-lang)
+                            (fire-particles:my-mod-lang)
+                            (spawners:my-mod-lang #:with-paren-runes? #t))
+	 (parentify 
+           flames
+           (spawner
+             gnarly-rock))
+	 spawner-demo.mp4)
+      }
+      )
+
+    )
   #:preview-image preview.png)
 
 
